d35b3105f44dd7db5fbc7230887f22f4ac6d8ad5
hoshikuro22 <superlaggerxz@gmail.com>
2024-02-05 23:32:44 +0800

    a

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8081/",
+  baseURL: "http://ched-records-app.vercel.app:8081/",
   withCredentials: true,
 });
 // export const makeRequest = axios.create({
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://ched-records-app.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
