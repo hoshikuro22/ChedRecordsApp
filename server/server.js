7d2f78c349c328b00d98690be0c72cd18dd9eaed
hoshikuro22 <superlaggerxz@gmail.com>
2024-02-08 14:20:51 +0800

    [

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://ched-records-app.vercel.app/"],
+    origin: ["http://localhost:5173"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
